79074e4dc92366c1a4b43c03b31afc6fdb2bfbff
ToonKru3 <supadett.pins@gmail.com>
2023-04-24 14:37:33 +0700

    adding images

--- www/vite.config.js
+++ www/vite.config.js
@@ -4,6 +4,9 @@ import glsl from "vite-plugin-glsl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../dist'
+  },
   base: "/mj-ribbon-demo/",
   plugins: [
     glsl({
