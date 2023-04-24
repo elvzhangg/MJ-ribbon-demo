7a03a0dc314809379f410f05bf168f5dafd5f592
ToonKru3 <supadett.pins@gmail.com>
2023-04-24 14:13:00 +0700

    adding folder dist

--- www/vite.config.js
+++ www/vite.config.js
@@ -4,7 +4,7 @@ import glsl from "vite-plugin-glsl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/mj-ribbon-demo/",
   plugins: [
     glsl({
       include: ["**/*.{glsl,vert,frag,vs,fs}"],
